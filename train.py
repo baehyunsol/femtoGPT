56102db7db419cfc4e2dbc8e3e0f10ba65248d64
baehyunsol <baehyunsol@naver.com>
2025-07-18 20:11:01 +0900

    refactor train.py

--- train.py
+++ train.py
@@ -1,208 +1,426 @@
-import json
 import os
 import random
 import shutil
 import subprocess
 import sys
-from typing import Tuple
+from typing import Optional, Tuple
 
-# 1. Instantiate X models with different seeds.
-# 2. Train each model Y steps.
-# 3. Sort the models by loss, and choose the top Z models (`m_1`, `m_2`, ... `m_z`).
-# 4. Duplicate the chosen models to X models and go back to step 1.
-#   - ex) if X = 5 and Z = 2, make a set: (`m_1`, `m_1`, `m_1`, `m_2`, `m_2`)
-def competitive_train(
-    prefix: str,  # name of the model
-    init_models: int,    # X
-    train_steps: int,    # Y
-    select_models: int,  # Z
+def write_string(path: str, content: str):
+    with open(path, "w") as f:
+        f.write(content)
 
-    # in the example above, it's [3, 2]
-    dup_count: list[int],
-    **hyperparameters,
+def train_and_checkpoint(
+    models: list[Tuple[str, str]],
+    steps: int,
+    dropout: float,
 ):
-    hyperparameters = [f"--{k.replace('_', '-')}={v}" for k, v in hyperparameters.items()]
-    models = [f"{prefix}-model-{random.randint(0, (1 << 40) - 1):010x}.dat" for _ in range(init_models)]
+    tmp_file_name = f"tmp-{random.randint(0, 0xffff_ffff):08x}.dat"
 
-    assert len(dup_count) == select_models
-    assert sum(dup_count) == init_models
-    assert all([c >= 1 for c in dup_count])
+    # femtoGPT saves a model every 10 steps
+    steps += 1
 
-    for i in range(100):
-        print(f"---- session {i}: step {i * train_steps} ~ {i * train_steps + train_steps - 1} ----")
-        new_models = []
-        loss_by_model = []
+    for a, b in models:
+        if os.path.exists(b):
+            continue
 
-        for model in models:
-            if i == 0:
-                subprocess.run(
-                    ["cargo", "run", "--release", "--", "init", *hyperparameters, "--model", model],
-                    capture_output=True,  # mute stdout
-                    check=True,
-                )
+        shutil.copyfile(a, tmp_file_name)
+        subprocess.run(
+            [
+                "./gpt", "train",
+                "--model", tmp_file_name,
+                "--steps", str(steps),
+                "--dropout", str(dropout),
+            ],
+            capture_output=True,
+            check=True,
+        )
+        os.rename(tmp_file_name, b)
 
+def train(
+    # You can distinguish different train sessions with a prefix
+    prefix: str = "",
+    dataset: str = "dataset.txt",
+
+    # Before you train, make sure to compile femtoGPT and
+    # place the binary file at this location
+    binary: str = "./gpt",
+
+    # ascii | char | bpe
+    tokenizer: str = "ascii",
+    reserve_tokens: int = 0,
+
+    # only for bpe tokenizer
+    vocab_size: int = 768,
+
+    # only for bpe tokenizer
+    # it dumps the stdout of train-bpe command
+    tokenizer_dump: Optional[str] = "tokenizer-dump.txt",
+
+    # count tokens in dataset and dump the result here
+    token_count_dump: Optional[str] = "token-count-dump.json",
+
+    # absolute | none
+    positional_encoding: str = "",
+    case_sensitive: bool = True,
+
+    # model hyperparameters
+    num_tokens: int = 80,
+    embedding_degree: int = 80,
+    num_layers: int = 4,
+    num_heads: int = 4,
+    dropout: float = 0.1,
+
+    # It initializes `init_models` models,
+    # trains each for `init_steps` steps,
+    # creates checkpoints,
+    # trains each for `mature_steps` steps,
+    # creates checkpoints,
+    # insert a layer to each model at `insert_layer_at` layer (`ext_models` times each),
+    # trains `ext_init_steps` steps,
+    # creates checkpoints,
+    # trains `ext_mature_steps` steps,
+    # creates checkpoints,
+    # chooses the best `ext2_models` models among `init_models` * `ext_models` models,
+    # insert a layer to the models at `insert_layer_at` layer (only once),
+    # trains `ext_init_steps` steps,
+    # creates checkpoints,
+    # trains `ext_mature_steps` steps,
+    # creates checkpoints,
+    # and boom!
+    init_models: int = 4,
+    init_steps: int = 200,
+    mature_steps: int = 500,
+    insert_layer_at: int = 2,
+    ext_models: int = 2,
+    ext_init_steps: int = 200,
+    ext_mature_steps: int = 500,
+    ext2_models: int = 3,
+):
+    assert tokenizer in ["ascii", "char", "bpe"]
+    assert positional_encoding in ["absolute", "none"]
+    tokenizer_data = dataset
+    case_sensitive = "--case-sensitive" if case_sensitive else "--case-insensitive"
+
+    if tokenizer == "bpe":
+        print("Initializing a tokenizer...")
+        # I don't want a name collision
+        tokenizer_data = f"tokenizer-{random.randint(0, 0xffff_ffff):08x}.json"
+        train_bpe = subprocess.run(
+            [
+                binary, "train-bpe",
+                case_sensitive,
+                "--tokenizer-data", tokenizer_data,
+                "--dataset", dataset,
+                "--reserve-tokens", str(reserve_tokens),
+                "--vocab-size", str(vocab_size),
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        if tokenizer_dump is not None:
+            write_string(tokenizer_dump, train_bpe.stdout)
+
+    if tokenizer == "bpe" and token_count_dump is not None:
+        count_tokens = subprocess.run(
+            [
+                binary, "count-tokens",
+                "--model", tokenizer_data,
+                "--dataset", dataset,
+            ],
+            capture_output=True,
+            text=True,
+        )
+        write_string(token_count_dump, count_tokens.stdout)
+
+    print(f"Initializing {init_models} models...")
+
+    for n in range(init_models):
+        subprocess.run(
+            [
+                binary, "init",
+                "--model", f"{prefix}exp{n}.dat",
+                "--tokenizer", tokenizer,
+                case_sensitive,
+                "--tokenizer-data", tokenizer_data,
+                "--positional-encoding", positional_encoding,
+                "--num-tokens", str(num_tokens),
+                "--embedding-degree", str(embedding_degree),
+                "--num-layers", str(num_layers),
+                "--num-heads", str(num_heads),
+            ],
+            capture_output=True,
+            check=True,
+        )
+
+    if tokenizer_data != dataset:
+        os.remove(tokenizer_data)
+
+    if tokenizer == "char" and token_count_dump is not None:
+        count_tokens = subprocess.run(
+            [
+                binary, "count-tokens",
+                "--model", f"{prefix}exp0.dat",
+                "--dataset", dataset,
+            ],
+            capture_output=True,
+            text=True,
+        )
+        write_string(token_count_dump, count_tokens.stdout)
+
+    print(f"Initial training {init_steps} steps...")
+    train_and_checkpoint(
+        models=[(f"{prefix}exp{n}.dat", f"{prefix}exp{n}-chk0.dat") for n in range(init_models)],
+        steps=init_steps,
+        dropout=dropout,
+    )
+
+    print("losses")
+
+    for n in range(init_models):
+        loss = subprocess.run(
+            [
+                binary, "loss",
+                "--model", f"{prefix}exp{n}-chk0.dat",
+                "--limit", "1",
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout.strip()
+        print(f"{prefix}exp{n}-chk0.dat: {loss}")
+
+    print(f"Training {mature_steps} steps...")
+    train_and_checkpoint(
+        models=[(f"{prefix}exp{n}-chk0.dat", f"{prefix}exp{n}-chk1.dat") for n in range(init_models)],
+        steps=mature_steps,
+        dropout=dropout,
+    )
+
+    print("losses")
+
+    for n in range(init_models):
+        loss = subprocess.run(
+            [
+                binary, "loss",
+                "--model", f"{prefix}exp{n}-chk1.dat",
+                "--limit", "1",
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout.strip()
+        print(f"{prefix}exp{n}-chk1.dat: {loss}")
+
+    print(f"Inserting a layer...")
+    losses = []
+
+    for n in range(init_models):
+        for m in range(ext_models):
             subprocess.run(
-                ["cargo", "run", "--release", "--", "train", "--steps", str(train_steps), "--dropout=0.1", "--model", model],
-                capture_output=True,  # mute stdout
+                [
+                    binary, "insert-layer",
+                    "--input", f"{prefix}exp{n}-chk1.dat",
+                    "--output", f"{prefix}exp{n}-ext{m}.dat",
+                    "--insert-at", str(insert_layer_at),
+                ],
+                capture_output=True,
                 check=True,
             )
-            info = subprocess.run(
-                ["cargo", "run", "--release", "--", "info", "--model", model],
+            train_and_checkpoint(
+                models=[(f"{prefix}exp{n}-ext{m}.dat", f"{prefix}exp{n}-ext{m}-chk0.dat")],
+                steps=ext_init_steps,
+                dropout=dropout,
+            )
+
+    for n in range(init_models):
+        for m in range(ext_models):
+            train_and_checkpoint(
+                models=[(f"{prefix}exp{n}-ext{m}-chk0.dat", f"{prefix}exp{n}-ext{m}-chk1.dat")],
+                steps=ext_mature_steps,
+                dropout=dropout,
+            )
+            loss = subprocess.run(
+                [
+                    binary, "loss",
+                    "--model", f"{prefix}exp{n}-ext{m}-chk1.dat",
+                    "--limit", "10",
+                ],
                 capture_output=True,
                 text=True,
                 check=True,
-            ).stdout
-            info = json.loads(info)
-            loss_by_model.append((model, info["logs"][-1]["kind"]["TrainStep"]["avg_loss"]))
+            )
+            losses.append((n, m, sum([eval(line) for line in loss.stdout.split("\n") if line]) / 10))
 
-        loss_by_model.sort(key=lambda m: m[1])
+    print("avg losses")
 
-        for rank, (model, loss) in enumerate(loss_by_model):
-            print(f"{model}: {loss}")
+    for n, m, loss in losses:
+        print(f"{prefix}exp{n}-ext{m}-chk1.dat: {loss}")
 
-            if rank < select_models:
-                # It saves checkpoints: the best model of each session
-                if rank == 0:
-                    shutil.copyfile(model, f"{prefix}-chk-{i}-{int(loss * 10000)}" + model)
+    print(f"Selecting {ext2_models} among {init_models * ext_models} models...")
+    losses.sort(key=lambda t: t[2])
+    losses = losses[:ext2_models]
 
-                new_models.append(model)
+    print(f"Inserting another layer...")
 
-                for _ in range(dup_count[rank] - 1):
-                    new_model = f"{prefix}-model-{random.randint(0, (1 << 40) - 1):010x}.dat"
-                    print(f"fork: {model} -> {new_model}")
-                    shutil.copyfile(model, new_model)
-                    new_models.append(new_model)
-
-            else:
-                os.remove(model)
-
-        models = new_models
-
-def incremental_train(
-    prefix: str,  # name of the model
-    pre_steps: int,
-    passes: list[Tuple[int, int]],  # (insert_at, steps)
-    post_steps: int,
-    **hyperparameters,
-):
-    hyperparameters = [f"--{k.replace('_', '-')}={v}" for k, v in hyperparameters.items()]
-
-    subprocess.run(
-        ["cargo", "run", "--release", "--", "init", *hyperparameters, "--model", f"{prefix}-model-0.dat"],
-        capture_output=True,  # mute stdout
-    )
-    subprocess.run(
-        ["cargo", "run", "--release", "--", "train", "--steps", str(pre_steps), "--dropout=0.1", "--model", f"{prefix}-model-0.dat"],
-        capture_output=True,  # mute stdout
-        check=True,
-    )
-    i = 0
-
-    while i < len(passes):
-        insert_at, steps = passes[i]
-        prev_model = f"{prefix}-model-{i}.dat"
-        next_model = f"{prefix}-model-{i + 1}.dat"
-
-        print(f"---- session {i} ----")
+    for n, m, _ in losses:
         subprocess.run(
-            ["cargo", "run", "--release", "--", "insert-layer", "--input", prev_model, "--output", next_model, "--insert-at", str(insert_at)],
-            capture_output=True,  # mute stdout
+            [
+                binary, "insert-layer",
+                "--input", f"{prefix}exp{n}-ext{m}-chk1.dat",
+                "--output", f"{prefix}exp{n}-ext{m}-ext0.dat",
+            ],
+            capture_output=True,
             check=True,
         )
-        subprocess.run(
-            ["cargo", "run", "--release", "--", "train", "--steps", str(steps), "--dropout=0.1", "--model", next_model],
-            capture_output=True,  # mute stdout
-            check=True,
+        train_and_checkpoint(
+            models=[(f"{prefix}exp{n}-ext{m}-ext0.dat", f"{prefix}exp{n}-ext{m}-ext0-chk0.dat")],
+            steps=ext_init_steps,
+            dropout=dropout,
         )
 
-        info = subprocess.run(
-            ["cargo", "run", "--release", "--", "info", "--model", prev_model],
+    new_losses = []
+
+    for n, m, _ in losses:
+        train_and_checkpoint(
+            models=[(f"{prefix}exp{n}-ext{m}-ext0-chk0.dat", f"{prefix}exp{n}-ext{m}-ext0-chk1.dat")],
+            steps=ext_mature_steps,
+            dropout=dropout,
+        )
+        loss = subprocess.run(
+            [
+                binary, "loss",
+                "--model", f"{prefix}exp{n}-ext{m}-ext0-chk1.dat",
+                "--limit", "10",
+            ],
             capture_output=True,
             text=True,
             check=True,
-        ).stdout
-        info = json.loads(info)
-        prev_loss = info["logs"][-1]["kind"]["TrainStep"]["avg_loss"]
-        info = subprocess.run(
-            ["cargo", "run", "--release", "--", "info", "--model", next_model],
+        )
+        new_losses.append((n, m, sum([eval(line) for line in loss.stdout.split("\n") if line]) / 10))
+
+    losses = new_losses
+    print("losses")
+
+    for n, m, loss in losses:
+        print(f"{prefix}exp{n}-ext{m}-ext0-chk1.dat: {loss}")
+
+    print(f"Selecting the best model among {len(losses)} models...")
+    losses.sort(key=lambda t: t[2])
+    n, m, _ = losses[0]
+
+    print("Beginning the infinite training step...")
+    train_and_checkpoint(
+        models=[(f"{prefix}exp{n}-ext{m}-ext0-chk1.dat", f"{prefix}final-chk0.dat")],
+        steps=init_steps,
+        dropout=dropout,
+    )
+
+    for i in range(999999):
+        train_and_checkpoint(
+            models=[(f"{prefix}final-chk{i}.dat", f"{prefix}final-chk{i + 1}.dat")],
+            steps=init_steps,
+            dropout=dropout,
+        )
+        loss = subprocess.run(
+            [
+                binary, "loss",
+                "--model", f"{prefix}final-chk{i}.dat",
+                "--limit", "10",
+            ],
             capture_output=True,
             text=True,
             check=True,
-        ).stdout
-        info = json.loads(info)
-        next_loss = info["logs"][-1]["kind"]["TrainStep"]["avg_loss"]
-        print(f"prev_loss: {prev_loss}, next_loss: {next_loss}")
-
-        if prev_loss < next_loss:
-            print("It seems like we have to run this session again")
-
-        else:
-            i += 1
-
-    subprocess.run(
-        ["cargo", "run", "--release", "--", "train", "--steps", str(post_steps), "--dropout=0.1", "--model", f"{prefix}-model-0.dat"],
-        capture_output=True,  # mute stdout
-        check=True,
-    )
+        )
+        loss = sum([eval(line) for line in loss.stdout.split("\n") if line]) / 10
+        print(f"{prefix}final-chk{i}.dat: {loss}")
 
 if __name__ == "__main__":
-    command = sys.argv[1]
-    prefix = sys.argv[2]
+    command = sys.argv[1] if len(sys.argv) > 1 else None
+    arg = sys.argv[2] if len(sys.argv) > 2 else None
 
-    if command == "comp-small":
-        competitive_train(
-            prefix=prefix,
-            init_models=5,
-            train_steps=200,
-            select_models=2,
-            dup_count=[3, 2],
+    # You have to edit these manually :(
+    tokenizer = "bpe"
+    vocab_size = 1024
+    reserve_tokens = 80
+    positional_encoding = "none"
+    case_sensitive = True
 
-            embedding_degree=80,
+    if command == "tiny":
+        train(
+            prefix=arg or "",
+            tokenizer=tokenizer,
+            reserve_tokens=reserve_tokens,
+            vocab_size=vocab_size,
+            positional_encoding=positional_encoding,
+            case_sensitive=case_sensitive,
+
+            embedding_degree=64,
             num_layers=4,
             num_heads=4,
+            init_models=2,
+            init_steps=30,
+            mature_steps=60,
+            insert_layer_at=2,
+            ext_models=1,
+            ext_init_steps=30,
+            ext_mature_steps=60,
+            ext2_models=2,
         )
 
-    elif command == "comp-medium":
-        competitive_train(
-            prefix=prefix,
-            init_models=5,
-            train_steps=800,
-            select_models=2,
-            dup_count=[3, 2],
+    elif command == "small":
+        train(
+            prefix=arg or "",
+            tokenizer=tokenizer,
+            reserve_tokens=reserve_tokens,
+            vocab_size=vocab_size,
+            positional_encoding=positional_encoding,
+            case_sensitive=case_sensitive,
 
-            embedding_degree=144,
-            num_layers=8,
-            num_heads=6,
+            # uses the default settings for the rest
         )
 
-    elif command == "inc-small":
-        incremental_train(
-            prefix=prefix,
-            pre_steps=200,
-            passes=[
-                (3, 100),
-                (3, 100),
-                (3, 100),
-                (3, 100),
-            ],
-            post_steps=100,
-            embedding_degree=80,
-            num_layers=4,
-            num_heads=4,
-        )
+    elif command == "medium":
+        train(
+            prefix=arg or "",
+            tokenizer=tokenizer,
+            reserve_tokens=reserve_tokens,
+            vocab_size=vocab_size,
+            positional_encoding=positional_encoding,
+            case_sensitive=case_sensitive,
 
-    elif command == "inc-medium":
-        incremental_train(
-            prefix=prefix,
-            pre_steps=500,
-            passes=[
-                (4, 250),
-                (4, 250),
-                (4, 250),
-                (4, 250),
-            ],
-            post_steps=300,
             embedding_degree=144,
             num_layers=6,
             num_heads=6,
+            init_models=4,
+            init_steps=500,
+            mature_steps=2000,
+            insert_layer_at=4,
+            ext_models=2,
+            ext_init_steps=1000,
+            ext_mature_steps=2000,
+            ext2_models=3,
+        )
+
+    elif command == "big":
+        train(
+            prefix=arg or "",
+            tokenizer=tokenizer,
+            reserve_tokens=reserve_tokens,
+            vocab_size=vocab_size,
+            positional_encoding=positional_encoding,
+            case_sensitive=case_sensitive,
+
+            embedding_degree=256,
+            num_layers=8,
+            num_heads=8,
+            init_models=4,
+            init_steps=1500,
+            mature_steps=6000,
+            insert_layer_at=6,
+            ext_models=2,
+            ext_init_steps=3000,
+            ext_mature_steps=6000,
+            ext2_models=3,
         )
